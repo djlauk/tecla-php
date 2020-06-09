f84dd8a27ef2ba57073f9fc4646e66beff5602d0
Daniel J. Lauk <daniel.lauk@gmail.com>
2020-06-09 17:32:56 +0200

    chore: disable errors in production mode
    Don't just assume, that error reporting is turned off.

--- src/index.php
+++ src/index.php
@@ -31,6 +31,9 @@ date_default_timezone_set($app['config.timezone']);
 if ($app['config.debug']) {
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
+} else {
+    error_reporting(0);
+    ini_set('display_errors', '0');
 }
 
 $app('session')->init();
