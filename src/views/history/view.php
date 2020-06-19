fbc2dba11f123cf82e8f1086121b5c7197dea6dc
Daniel J. Lauk <daniel.lauk@gmail.com>
2020-06-19 09:18:09 +0200

    feat: visualize changes in object history
    closes #53

--- src/views/history/view.php
+++ src/views/history/view.php
@@ -6,11 +6,22 @@
 // tecla is open source under the terms of the MIT license.
 // For details see LICENSE.md.
 // ----------------------------------------------------------------------
+
 ?>
 
 <h1>History of <?=strtoupper($type)?>:<?=$id?></h1>
 
-<?php foreach ($entries as $e): ?>
+<?php
+$maxIdx = count($entries) - 1;
+foreach ($entries as $idx => $e): ?>
+<?php
+$data = json_decode($e->data, true);
+$prevData = $idx < $maxIdx ? json_decode($entries[$idx + 1]->data, true) : array();
+?>
 <h2>Version <?=$e->version?> (<?=$e->metaCreatedOn->format('Y-m-d H:i:s')?>)</h2>
-<pre><?=htmlentities($e->data)?></pre>
+<table>
+<?php foreach ($data as $k => $v): ?>
+    <tr class="<?=$prevData[$k] == $data[$k] ? 'unchanged' : 'changed'?>"><td><?=htmlentities($k)?></td><td><?=htmlentities($v)?></td></th>
+<?php endforeach?>
+</table>
 <?php endforeach?>
