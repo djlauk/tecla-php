524a91b8be85b03bb29cca7288f9b449fefa5e1f
Daniel J. Lauk <daniel.lauk@gmail.com>
2020-07-06 09:20:16 +0200

    chore: remove additional guest accounts
    Experience has shown, that just having 1 guest account works well enough for daily operations.
    In fact multiple guest accounts have been perceived as confusing.

--- db/03-create-guest-users.sql
+++ db/03-create-guest-users.sql
@@ -1,4 +1,4 @@
--- initial guest accounts
+-- initial guest account
 
 INSERT INTO `users` (
     `displayName`,
@@ -10,50 +10,13 @@ INSERT INTO `users` (
     `lockedUntil`,
     `metaVersion`
 ) VALUES (
-	'Guest 1',
+	'Guest',
 	'',  -- **this** password will never match anything
-	'guest1',
+	'guest',
 	'guest',
 	NULL,  -- not disabled, so it can be used when booking
 	NULL,
-	'9999-12-31 23:59:59',
+	'9999-12-31 23:59:59', -- but locked out for ever, os it can't log in
 	1
 );
-INSERT INTO `users` (
-    `displayName`,
-    `passwordHash`,
-    `email`,
-    `role`,
-    `disabledOn`,
-    `verifiedOn`,
-		`lockedUntil`,
-    `metaVersion`
-) VALUES (
-	'Guest 2',
-	'',  -- **this** password will never match anything
-	'guest2',
-	'guest',
-	NULL,  -- not disabled, so it can be used when booking
-	NULL,
-	'9999-12-31 23:59:59',
-	1
-);
-INSERT INTO `users` (
-    `displayName`,
-    `passwordHash`,
-    `email`,
-    `role`,
-    `disabledOn`,
-    `verifiedOn`,
-		`lockedUntil`,
-    `metaVersion`
-) VALUES (
-	'Guest 3',
-	'',  -- **this** password will never match anything
-	'guest3',
-	'guest',
-	NULL,  -- not disabled, so it can be used when booking
-	NULL,
-	'9999-12-31 23:59:59',
-	1
-);
+
